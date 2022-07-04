9f2df22fd5b01b95baa7474a49b8969319c97ac7
Matthieu COISNON <matthieu.coisnon@decathlon.fr>
2022-07-04 14:32:56 +0200

    Add maven wrapper in project

---
+++ src/test/java/com/decathlon/creation/poctaxotool/PocTaxotoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decathlon.creation.poctaxotool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocTaxotoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
