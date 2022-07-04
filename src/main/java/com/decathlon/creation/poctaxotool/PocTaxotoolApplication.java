992934f7483dfbe3c22f164ecfc762ea53164ad8
Matthieu COISNON <matthieu.coisnon@decathlon.fr>
2022-07-04 14:31:53 +0200

    INIT project

---
+++ src/main/java/com/decathlon/creation/poctaxotool/PocTaxotoolApplication.java
@@ -0,0 +1,13 @@
+package com.decathlon.creation.poctaxotool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocTaxotoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocTaxotoolApplication.class, args);
+	}
+
+}
